84b937dbf671a2340733b2261a2d8b6c490d71fe
AJ Singh <armaansingh04@proton.me>
2024-12-06 18:58:32 +0000

    Edited how VPN services are installed

--- modules/nixos/user-sys-pkgs.nix
+++ modules/nixos/user-sys-pkgs.nix
@@ -1,18 +1,29 @@
 { config, pkgs, ... }:
 
 let 
-    hostname  = config.networking.hostName;
-in
-{
-  environment.systemPackages = with pkgs; [ 
-    syncthing 
-    (if hostname == "qpc" then mullvad-vpn else protonvpn-gui)
+  hostname = config.networking.hostName;
+  # Define the list of packages to install
+  packages = with pkgs; [
+    syncthing
+    (if hostname == "x280" then pkgs.protonvpn-gui else null)
   ];
 
-  #Syncthing
+  # Remove the null value from the list of packages
+  finalPackages = builtins.filter (pkg: pkg != null) packages;
+in
+{
+  environment.systemPackages = finalPackages;
+
+  # Conditionally enable Mullvad VPN if hostname is "qpc"
+  services.mullvad-vpn = if hostname == "qpc" then {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  } else null;
+
+  # Syncthing service configuration
   services.syncthing = {
-	  enable = true;
-	  user = (if hostname == "qpc" then "rhf" else "asingh");
-	  group = "users";
+    enable = true;
+    user = if hostname == "qpc" then "rhf" else "asingh";
+    group = "users";
   };
-}  
+}
